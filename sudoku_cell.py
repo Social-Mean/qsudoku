5809193fc0db5f67db0e64e17b0be21c854f2919
ming <2052760@tongji.edu.cn>
2025-07-01 22:56:19 +0800

    将 SudokuCell 类从 QLineEdit 更改为 QWidget，并添加自定义绘制功能以绘制单元格边框

--- sudoku_cell.py
+++ sudoku_cell.py
@@ -1,18 +1,20 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QLineEdit, QSizePolicy
 
 
-class SudokuCell(QLineEdit):
+class SudokuCell(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.setMinimumSize(30, 30)
 
-    def set_value(self, value):
-        self.setText(str(value))
+    def paintEvent(self, event):
+        from PySide6.QtGui import QPainter, QPen
+        from PySide6.QtCore import Qt
 
-    def get_value(self):
-        return int(self.text())
+        painter = QPainter(self)
+        pen = QPen(Qt.black, 2)  # 1像素黑色画笔
+        painter.setPen(pen)
 
-    def set_editable(self, editable):
-        self.setReadOnly(not editable)
+        # 绘制正方形边框
+        painter.drawRect(0, 0, self.width(), self.height())
