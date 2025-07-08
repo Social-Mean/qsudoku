3673d431feb015989f53815ca1de5b518f223a20
ming <2052760@tongji.edu.cn>
2025-07-08 16:47:55 +0800

    为 SudokuCell 类添加了数字绘制功能，重构了绘制逻辑，增加了边框和数字的绘制方法。

--- sudoku_cell.py
+++ sudoku_cell.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QWidget, QSizePolicy
-from PySide6.QtGui import QPainter, QPen
+from PySide6.QtGui import QPainter, QPen, QFont
 from PySide6.QtCore import QRect, Qt
 
 
@@ -10,16 +10,25 @@ class SudokuCell(QWidget):
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.setMinimumSize(30, 30)
 
-        self.number = 0
+        self.number = 1
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        pen = QPen(Qt.black, 2)  # 1像素黑色画笔
-        painter.setPen(pen)
-
         rect = QRect(0, 0, self.width(), self.height())
+        self._draw_border(painter, rect)
+        self._draw_number(painter, rect)
+
+    def side(self):
+        return self.width()
+
+    def _draw_border(self, painter: QPainter, rect: QRect):
+        pen = QPen(Qt.black, 2)
+        painter.setPen(pen)
         painter.drawRect(rect)
 
+    def _draw_number(self, painter: QPainter, rect: QRect):
+        font_size = self.side() * 0.618
+        painter.setFont(QFont("Arial", font_size))
         if self.number > 0:
             painter.drawText(rect, Qt.AlignCenter, str(self.number))
 
